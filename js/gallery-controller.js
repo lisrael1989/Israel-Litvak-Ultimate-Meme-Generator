3445d05159f7d6b9103fdefad80a43ef434426eb
israel <lisrael1989@gmail.com>
2024-03-01 09:54:49 +0200

    add filltering functions

--- js/gallery-controller.js
+++ js/gallery-controller.js
@@ -1,7 +1,31 @@
 "use strict";
 
-renderGallery();
+var gFilterBy = {
+  keywords: "",
+};
 
+var gImgs = [
+  { id: 1, url: "meme-imgs/1.jpg", keywords: ["famous", "tramp"] },
+  { id: 2, url: "meme-imgs/2.jpg", keywords: ["pets", "dog"] },
+  { id: 3, url: "meme-imgs/3.jpg", keywords: ["funny", "cat"] },
+  { id: 4, url: "meme-imgs/4.jpg", keywords: ["pets", "cat"] },
+  { id: 5, url: "meme-imgs/5.jpg", keywords: ["funny", "baby"] },
+  { id: 6, url: "meme-imgs/6.jpg", keywords: ["funny", "cat"] },
+  { id: 7, url: "meme-imgs/7.jpg", keywords: ["funny", "kids"] },
+  { id: 8, url: "meme-imgs/8.jpg", keywords: ["funny", "movie"] },
+  { id: 9, url: "meme-imgs/9.jpg", keywords: ["famous", "kids"] },
+  { id: 10, url: "meme-imgs/10.jpg", keywords: ["funny", "cat"] },
+  { id: 11, url: "meme-imgs/11.jpg", keywords: ["funny", "movie"] },
+  { id: 12, url: "meme-imgs/12.jpg", keywords: ["funny", "cat"] },
+  { id: 13, url: "meme-imgs/13.jpg", keywords: ["famous", "funny"] },
+  { id: 14, url: "meme-imgs/14.jpg", keywords: ["movie", "scary"] },
+  { id: 15, url: "meme-imgs/15.jpg", keywords: ["famous", "cat"] },
+  { id: 16, url: "meme-imgs/16.jpg", keywords: ["movie", "cat"] },
+  { id: 17, url: "meme-imgs/17.jpg", keywords: ["famous", "dictator"] },
+  { id: 18, url: "meme-imgs/18.jpg", keywords: ["movie", "funny"] },
+];
+
+renderGallery();
 function renderGallery() {
   const elGallery = document.querySelector(".imgs-container");
   const imgs = getImg();
@@ -15,8 +39,31 @@ function renderGallery() {
   elGallery.innerHTML = strHTML;
 }
 
+function getImg() {
+  if (!gFilterBy.keywords) return gImgs;
+
+  const filteredImgs = gImgs.filter((img) =>
+    img.keywords.some((keyword) =>
+      keyword.toLowerCase().includes(gFilterBy.keywords.toLowerCase())
+    )
+  );
+
+  return filteredImgs;
+}
+
 function onSelectImg(id) {
   setImg(id);
   renderMeme(gMeme);
   switchSection();
+}
+
+/* fillter Keywords*/
+
+function onSetFilterBy(filterBy) {
+  setFilterBy(filterBy);
+  renderGallery();
+}
+
+function setFilterBy(filterBy) {
+  if (filterBy.keywords !== undefined) gFilterBy.keywords = filterBy.keywords;
 }
--- js/service/meme.service.js
+++ js/service/meme.service.js
@@ -1,26 +1,6 @@
 "use strict";
 
 const MEME_DB = "memesDB";
-var gImgs = [
-  { id: 1, url: "meme-imgs/1.jpg", keywords: ["famous", "tramp"] },
-  { id: 2, url: "meme-imgs/2.jpg", keywords: ["pets", "dog"] },
-  { id: 3, url: "meme-imgs/3.jpg", keywords: ["funny", "cat"] },
-  { id: 4, url: "meme-imgs/4.jpg", keywords: ["pets", "cat"] },
-  { id: 5, url: "meme-imgs/5.jpg", keywords: ["funny", "baby"] },
-  { id: 6, url: "meme-imgs/6.jpg", keywords: ["funny", "cat"] },
-  { id: 7, url: "meme-imgs/7.jpg", keywords: ["funny", "children"] },
-  { id: 8, url: "meme-imgs/8.jpg", keywords: ["funny", "movie"] },
-  { id: 9, url: "meme-imgs/9.jpg", keywords: ["famous", "children"] },
-  { id: 10, url: "meme-imgs/10.jpg", keywords: ["funny", "cat"] },
-  { id: 11, url: "meme-imgs/11.jpg", keywords: ["funny", "movie"] },
-  { id: 12, url: "meme-imgs/12.jpg", keywords: ["funny", "cat"] },
-  { id: 13, url: "meme-imgs/13.jpg", keywords: ["famous", "funny"] },
-  { id: 14, url: "meme-imgs/14.jpg", keywords: ["movie", "scary"] },
-  { id: 15, url: "meme-imgs/15.jpg", keywords: ["famous", "cat"] },
-  { id: 16, url: "meme-imgs/16.jpg", keywords: ["movie", "cat"] },
-  { id: 17, url: "meme-imgs/17.jpg", keywords: ["famous", "putin"] },
-  { id: 18, url: "meme-imgs/18.jpg", keywords: ["movie", "funny"] },
-];
 
 var gMeme = {
   selectedImgId: null,
@@ -45,10 +25,6 @@ var gMeme = {
 
 function getMeme() {
   return gMeme;
-}
-
-function getImg() {
-  return gImgs;
 }
 
 function getImgByIdx(idx = null) {
